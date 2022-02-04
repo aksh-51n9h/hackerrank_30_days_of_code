826b4f947d27253b770a1dba77252a7f5fc00bec
aksh-51n9h <singh2104.akshay@gmail.com>
2022-02-04 14:14:07 +0530

    solve Day 1

--- src/Day1.java
+++ src/Day1.java
@@ -1,5 +1,22 @@
-class Day1{
+import java.util.Scanner;
+
+public class Day1 {
     public static void main(String[] args) {
-        
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        int x = scan.nextInt();
+        double y = scan.nextDouble();
+        scan.nextLine();
+        String z = scan.nextLine();
+
+        System.out.println(i + x);
+        System.out.println(d + y);
+        System.out.println(s + z);
+
+        scan.close();
     }
 }
