264d6d6e5436f0581c1c31c158d6e68e62dc2096
aksh-51n9h <singh2104.akshay@gmail.com>
2022-02-04 14:11:43 +0530

    solve Day 0

---
+++ src/Day0.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Day0 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String inputString = scan.nextLine();
+        System.out.println("Hello, World.\n"+inputString);
+        scan.close();
+    }
+}
